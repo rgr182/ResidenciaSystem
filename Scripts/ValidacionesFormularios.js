272962360a443e25dbc1051681eb423d2822653b
SIEENA\raul.galvan <raul.galvan@definityfirst.com>
2016-10-10 18:03:05 -0500

    correcciones en script de validacion y se agrego funcionalidad para que solo haga submit al pasar la validacion

--- Scripts/ValidacionesFormularios.js
+++ Scripts/ValidacionesFormularios.js
@@ -3,23 +3,38 @@ var valida_empleado = function (){
 var selector_Contraseña = $('input[name="contra"]');
 var selector_Nombre =  $('input[name="nombre"]');
 var selector_Idempleado = $('input[name="idempleado"]');
+var validacion = true;
+	if (!selector_Contraseña.val()) { 
+	    alert("Constraseña esta vacia");
+	    validacion = false;
+	  }
 
-	if (!selector_Contraseña.value) { 
-	    alert("Constraseña esta vacia");	}
-	if(typeof(selector_Contraseña.value) != 'Number'){
-    	alert("revisa el formato del nombre solo pueden ir numeros");}
+/*	if(typeof(selector_Contraseña.val()) != 'number'){
+    	alert("revisa el formato de la contraseña solo pueden ir numeros");
+    	 validacion = false;
+    } */
 
-	if (!selector_Nombre.value) { 
+	if (!selector_Nombre.val()) { 
 	    alert("Nombre del empleado esta vacio");
-	}
-	if(typeof(selector_Contraseña.value) != 'String'){
-    	alert("revisa el formato del nombre solo pueden ir letras");
+	     validacion = false;
 	}
 
-	if (!selector_Idempleado.value) { 
+	/*if(typeof(selector_Contraseña.val()) != 'string'){
+    	alert("revisa el formato del nombre solo pueden ir letras");
+    	 validacion = false;
+	}*/
+
+	if (!selector_Idempleado.val()) { 
 	    alert("Numero del empleado esta vacio");
+	     validacion = false;
 	}
-	if(typeof(selector_Contraseña.value) != 'Number'){
-    	alert("revisa el formato del nombre solo pueden ir numeros");
+
+	/*if(typeof(selector_Contraseña.val()) != 'number'){
+    	alert("revisa el formato de la  Constraseña solo pueden ir numeros");
+    	 validacion = false;
+	}*/
+
+	if (validacion) {
+		$('#guardaEmpleado').submit()
 	}
 };
