c13d8df1eb5f8c3fb112b6c9e03d4bb0c6f63a23
SIEENA\raul.galvan <raul.galvan@definityfirst.com>
2016-10-10 21:53:34 -0500

    Revert "se agregaron mas validaciones para los demas campos de momento solo muestra mensaje y no detiene ejecucion"
    This reverts commit 6448c60c577611af3251333e38748dac3a6b1072.

--- Scripts/ValidacionesFormularios.js
+++ Scripts/ValidacionesFormularios.js
@@ -1,25 +1,11 @@
+
+
 var valida_empleado = function (){
-//Valida la contraseña
-var selector_Contraseña = $('input[name="contra"]');
-var selector_Nombre =  $('input[name="nombre"]');
-var selector_Idempleado = $('input[name="idempleado"]');
-
-	if (!selector_Contraseña.value) { 
-	    alert("Constraseña esta vacia");	}
-	if(typeof(selector_Contraseña.value) != 'Number'){
-    	alert("revisa el formato del nombre solo pueden ir numeros");}
-
-	if (!selector_Nombre.value) { 
+var selector = $('input[name="contra"]');
+	if (!selector.value) { 
 	    alert("Nombre del empleado esta vacio");
 	}
-	if(typeof(selector_Contraseña.value) != 'String'){
+	if(typeof(selector.value) != 'String'){
     	alert("revisa el formato del nombre solo pueden ir letras");
-	}
-
-	if (!selector_Idempleado.value) { 
-	    alert("Numero del empleado esta vacio");
-	}
-	if(typeof(selector_Contraseña.value) != 'Number'){
-    	alert("revisa el formato del nombre solo pueden ir numeros");
 	}
 };
