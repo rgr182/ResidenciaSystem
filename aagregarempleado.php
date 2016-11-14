3c1a6700f4ba4cc5ce4bc227e39f681ed00bac84
dulcevidales24 <11041448@itdurango.edu.mx>
2016-11-14 14:09:49 -0600

    EN  ESTOS DOS FORMULARIOS GUARDA LA "IMAGEN"OSEASE LAS LETRAS  PARA LA IMAGEN DEL EMPLEADO

--- aagregarempleado.php
+++ aagregarempleado.php
@@ -1,18 +1,13 @@
 <?php
  include 'conexion.php';
  $idempleado =$_POST['idempleado'];
-      $nombredelauditor =$_POST['nombredelauditor'];
+      $nombre =$_POST['nombre'];
       $contra =$_POST['contra'];
-      $tipousuario=$_post['tipousuario'];
-      $query1="INSERT INTO tablatipousuario (tipousuario) VALUES ('$tipousuario')";
-       $query1="INSERT INTO tablaempleado (idempleado, nombredelauditor,contra) VALUES ('$idempleado', '$nombredelauditor', '$contra')";
- $resultado= mysqli_query($conexion,$query1);
+      $tipousuario=$_POST['tipousuario'];
+      
+    $query1="INSERT INTO tablaempleado (idempleado, nombre,contra,imagenemp,tipousuario) VALUES ('$idempleado', '$nombre', '$contra','dulllce','$tipousuario')";
+  mysqli_query($conexion,$query1);
  mysqli_close($conexion);
- if ($resultado){
- 	header('Location:Administrador.php?pros=si');
- }else{
- 	header('Location:Administrador.php?pros=no');
- }
 
 
 
