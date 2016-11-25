e1e8027a8a312fab5abfa55a633fdeecdb0bde57
Raul Galvan <raul.galvan@definityfirst.com>
2016-11-25 12:43:42 -0600

    fix para evitar que se guarden empleados duplicados

--- aagregarempleado.php
+++ aagregarempleado.php
@@ -2,18 +2,35 @@
  include 'conexion.php';
 
  $idempleado =$_POST['idempleado'];
-      $nombre =$_POST['nombre'];
-      $contra =$_POST['contra'];
-      $tipousuario=$_POST['tipousuario'];
-      $target_path = "Imagenes/";     
-      $target_path = $target_path.basename( $_FILES['imagenemp']['name']);      
-      if(move_uploaded_file($_FILES['imagenemp']['tmp_name'], $target_path)) { 
-        echo "El archivo ". basename( $_FILES['imagenemp']['name']). " ha sido subido";
-      } else{
-          echo "Ha ocurrido un error, trate de nuevo!";
+ $nombre =$_POST['nombre'];
+ $contra =$_POST['contra'];
+ $tipousuario=$_POST['tipousuario'];
+ $target_path = "Imagenes/";     
+ $target_path = $target_path.basename( $_FILES['imagenemp']['name']);      
+ move_uploaded_file($_FILES['imagenemp']['tmp_name'], $target_path); 
+
+  $selectByName = "SELECT * FROM tablaempleado WHERE nombre LIKE '%$nombre%'";
+  $selectById   = "SELECT * FROM tablaempleado WHERE idempleado = '$idempleado'";   
+  $queryNombre  = mysqli_query($conexion,$selectByName);
+  $queryId      = mysqli_query($conexion,$selectById);
+
+ 
+ $contadorNombre = mysqli_num_rows ($queryNombre);
+ $contadorId = mysqli_num_rows ($queryId);
+  if ($contadorNombre  > 0 || $contadorId  > 0 ) {
+    echo "Ya existe ese Empleado";
+  }  
+
+  $guardarempleado="INSERT INTO tablaempleado (idempleado, nombre,contra,imagenemp,tipousuario) VALUES ('$idempleado', '$nombre', '$contra','$target_path','$tipousuario')";
+
+  if ($contadorNombre  <= 0) {
+      mysqli_query($conexion,$guardarempleado);
+      if (mysqli_query($conexion,$guardarempleado)) {
+      	echo "Empleado guardado Exitosamente";
       }
-      
-    $guardarempleado="INSERT INTO tablaempleado (idempleado, nombre,contra,imagenemp,tipousuario) VALUES ('$idempleado', '$nombre', '$contra','$target_path','$tipousuario')";
-  mysqli_query($conexion,$guardarempleado);
- mysqli_close($conexion);
+  }else{
+  	  echo "</br>Empleado no guardado";
+  }
+
+  mysqli_close($conexion);
 ?>
